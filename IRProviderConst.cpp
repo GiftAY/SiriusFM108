3c46707198b774fdbc77d44ef767c2cc5d0a0fc2
Andrey Yuganov <yugandrey@yandex.ru>
2021-02-10 09:45:24 +0300

    fourth commit

--- IRProviderConst.cpp
+++ IRProviderConst.cpp
@@ -6,14 +6,12 @@ namespace SiriusFm
 
 	IRProvider<IRModeE::Const>::IRProvider(char const* a_file){
 
-	/*if(a_file == nullptr || *a_file =='\0')
-		return;*/
+	if(a_file == nullptr || *a_file =='\0')
+		return;
 
 	FILE *fp = fopen("File.txt", "r");
 		char buff[128] = {0};
 		char name[4] = {0};
-	if(a_file == nullptr || *a_file =='\0')
-		return;
 
 		
 		//while(fgets(buff,128, fp) != nullptr){
--- main.cpp
+++ main.cpp
@@ -7,4 +7,8 @@ int main()
 	IRProvider<IRModeE::Const> irp("File.txt");
 	std::cout << irp.r(CcyE::RUB, 1) << "\n";
 	
+
+
+
+
 }
